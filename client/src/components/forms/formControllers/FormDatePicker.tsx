2c94d8dc379b6adeed21813842cba3421fd00137
Abdulaziz <alsheha88@gmail.com>
2026-09-10 21:38:04 +0300

    controller type change

--- client/src/components/forms/formControllers/FormDatePicker.tsx
+++ client/src/components/forms/formControllers/FormDatePicker.tsx
@@ -10,7 +10,7 @@ import { useState } from "react";
 import { formatDateShort } from "../../../lib/utils";
 
 type FormDatePickerProps<T extends FieldValues> = {
-	control: Control<T>;
+	control: Control<T, any, T>;
 	name: FieldPath<T>;
 	label: string;
 };
