b6c993ecfc08bbaf4a0bdd379ad0cc5b02e45a5d
Abdulaziz <alsheha88@gmail.com>
2026-09-10 23:16:15 +0300

    install and connect resend

--- server/lib/email.ts
+++ server/lib/email.ts
@@ -1,20 +1,13 @@
-//test
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
 
-const transporter = nodemailer.createTransport({
-	host: "smtp.gmail.com",
-	port: 465,
-	secure: true,
-	family: 4,
-	auth: { user: process.env.SMTP_EMAIL, pass: process.env.SMTP_PASS },
-} as nodemailer.TransportOptions);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendVerificationEmail = async (to: string, rawToken: string) => {
-	const link = `${process.env.APP_URL}/auth/verify-email?token=${rawToken}`;
-	return transporter.sendMail({
-		from: `Personal Finance App <${process.env.SMTP_EMAIL}>`,
-		to,
-		subject: "Verify your email",
-		html: `Click <a href="${link}">here</a> to verify your email.`,
-	});
+  const link = `${process.env.APP_URL}/auth/verify-email?token=${rawToken}`;
+  return resend.emails.send({
+    from: "Personal Finance App <onboarding@resend.dev>",
+    to,
+    subject: "Verify your email",
+    html: `Click <a href="${link}">here</a> to verify your email.`,
+  });
 };
